eed2def919650cedde4fad7695d148a7a63ec885
Jose Alejandro Montenegro Ruiz <josemr2001@gmail.com>
2025-03-14 18:21:54 -0600

    Lo siento profe

--- sw.js
+++ sw.js
@@ -1,32 +1,37 @@
-const CACHE_NAME = " convertidor de temperatura-v1";
+const CACHE_NAME = "convertidor-de-temperatura-v1";
 
-self.addEventListener('install',event =>{
-    event.waitUntil((async()=>{
-        const cache = await cache.open(CACHE_NAME);
-        cache.addsAll([
-            './',
-            //'./index.html',
-            './js/converter.js',
-            './css/style.css'
-        ]);
-    }))();
+self.addEventListener('install', event => {
+    // Usamos event.waitUntil para asegurarnos de que el Service Worker se instale correctamente
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll([
+                './',
+                './js/converter.js',
+                './css/style.css'
+            ]);
+        })
+    );
 });
 
-self.addEventListener('fetch',event=>{
-    event.respondwith((async()=>{
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(event.request);
-
-        if(cachedResponse){
-            return cachedResponse;
-        }else{
-            try{
-                const fetchResponse = await fetch(event.request);
-                cache.put(event.request,fetchResponse.clone());
-                return fetchResponse;
-            }catch(e){
-                //Hubo problema de red de datos.
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cachedResponse => {
+            // Si hay una respuesta en caché, la devolvemos
+            if (cachedResponse) {
+                return cachedResponse;
             }
-        }
-    })());
-})
+
+            // Si no está en caché, hacemos la solicitud de red
+            return fetch(event.request).then(fetchResponse => {
+                // Almacenamos la respuesta en caché y la devolvemos
+                return caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            }).catch(() => {
+                // Si ocurre un error en la red, devolvemos un mensaje alternativo
+                return new Response('No se puede acceder a la red.');
+            });
+        })
+    );
+});
